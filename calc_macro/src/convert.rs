ad9fce2a0648ff450530ee46bbbb43dcb24ad30a
Henry Till <henrytill@gmail.com>
2025-03-12 19:17:13 -0700

    calc_macro::convert: rewrite expr_to_syntax in iterative style

--- calc_macro/src/convert.rs
+++ calc_macro/src/convert.rs
@@ -4,50 +4,90 @@ use quote::quote;
 use calc::syntax::Expr;
 
 pub fn expr_to_syntax(expr: &Expr) -> TokenStream {
-    match expr {
-        Expr::Var(x) => quote! {
-            calc::syntax::Expr::Var(String::from(#x))
-        },
-        Expr::Const(n) => quote! {
-            calc::syntax::Expr::Const(#n)
-        },
-        Expr::Neg(a) => {
-            let a = expr_to_syntax(a);
-            quote! {
-                calc::syntax::Expr::Neg(Box::new(#a))
+    // Define a stack item to track nodes and their visit status
+    struct StackItem<'a> {
+        expr: &'a Expr,
+        visited: bool,
+    }
+
+    let mut stack = vec![StackItem { expr, visited: false }];
+    let mut results = Vec::new();
+
+    while let Some(item) = stack.pop() {
+        if item.visited {
+            // Process node after children have been processed
+            let result = match item.expr {
+                Expr::Neg(_) => {
+                    let a = results.pop().unwrap();
+                    quote! {
+                        calc::syntax::Expr::Neg(Box::new(#a))
+                    }
+                }
+                Expr::Add(_, _) => {
+                    let b = results.pop().unwrap();
+                    let a = results.pop().unwrap();
+                    quote! {
+                        calc::syntax::Expr::Add(Box::new(#a), Box::new(#b))
+                    }
+                }
+                Expr::Sub(_, _) => {
+                    let b = results.pop().unwrap();
+                    let a = results.pop().unwrap();
+                    quote! {
+                        calc::syntax::Expr::Sub(Box::new(#a), Box::new(#b))
+                    }
+                }
+                Expr::Mul(_, _) => {
+                    let b = results.pop().unwrap();
+                    let a = results.pop().unwrap();
+                    quote! {
+                        calc::syntax::Expr::Mul(Box::new(#a), Box::new(#b))
+                    }
+                }
+                Expr::Exp(_, _) => {
+                    let b = results.pop().unwrap();
+                    let a = results.pop().unwrap();
+                    quote! {
+                        calc::syntax::Expr::Exp(Box::new(#a), Box::new(#b))
+                    }
+                }
+                // These cases are handled directly when not visited
+                _ => panic!("We shouldn't be here"),
+            };
+            results.push(result);
+        } else {
+            // Process node on first encounter
+            match item.expr {
+                Expr::Add(a, b) | Expr::Sub(a, b) | Expr::Mul(a, b) | Expr::Exp(a, b) => {
+                    // Push this node back as visited for future processing
+                    stack.push(StackItem { expr: item.expr, visited: true });
+                    // Push children in reverse order (b then a) so a is processed first
+                    stack.push(StackItem { expr: b, visited: false });
+                    stack.push(StackItem { expr: a, visited: false });
+                }
+                Expr::Neg(a) => {
+                    stack.push(StackItem { expr: item.expr, visited: true });
+                    stack.push(StackItem { expr: a, visited: false });
+                }
+                // For leaf nodes, process them immediately
+                Expr::Var(x) => {
+                    results.push(quote! {
+                        calc::syntax::Expr::Var(String::from(#x))
+                    });
+                }
+                Expr::Const(n) => {
+                    results.push(quote! {
+                        calc::syntax::Expr::Const(#n)
+                    });
+                }
+                Expr::Metavar(s) => {
+                    let ident = syn::Ident::new(s, proc_macro2::Span::call_site());
+                    results.push(quote! { #ident });
+                }
             }
-        }
-        Expr::Add(a, b) => {
-            let a = expr_to_syntax(a);
-            let b = expr_to_syntax(b);
-            quote! {
-                calc::syntax::Expr::Add(Box::new(#a), Box::new(#b))
-            }
-        }
-        Expr::Sub(a, b) => {
-            let a = expr_to_syntax(a);
-            let b = expr_to_syntax(b);
-            quote! {
-                calc::syntax::Expr::Sub(Box::new(#a), Box::new(#b))
-            }
-        }
-        Expr::Mul(a, b) => {
-            let a = expr_to_syntax(a);
-            let b = expr_to_syntax(b);
-            quote! {
-                calc::syntax::Expr::Mul(Box::new(#a), Box::new(#b))
-            }
-        }
-        Expr::Exp(a, b) => {
-            let a = expr_to_syntax(a);
-            let b = expr_to_syntax(b);
-            quote! {
-                calc::syntax::Expr::Exp(Box::new(#a), Box::new(#b))
-            }
-        }
-        Expr::Metavar(s) => {
-            let ident = syn::Ident::new(s, proc_macro2::Span::call_site());
-            quote! { #ident }
         }
     }
+
+    // The final result should be the only item in the results vector
+    results.pop().unwrap()
 }
