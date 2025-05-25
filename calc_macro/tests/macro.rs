a57ad6f3cdc8076ca169fa256cf174a77ccdf156
Henry Till <henrytill@gmail.com>
2025-05-24 20:30:44 -0700

    calc_macro: revamp tests

--- calc_macro/tests/macro.rs
+++ calc_macro/tests/macro.rs
@@ -1,243 +1,142 @@
-use calc::syntax::*;
+use calc::parser;
 use calc_macro::calc;
 
 #[test]
 fn test_basic_expr() {
+    let expected = parser::parse_expr("2 * x + 1").unwrap();
     let actual = calc!("2 * x + 1");
-    let expected = Expr::Add(
-        Box::new(Expr::Mul(Box::new(Expr::Const(2)), Box::new(Expr::Var("x".to_string())))),
-        Box::new(Expr::Const(1)),
-    );
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_antiquote() {
-    let x = Expr::Add(Box::new(Expr::Var("y".to_string())), Box::new(Expr::Var("z".to_string())));
+    let expected = parser::parse_expr("2 * (y + z) + 1").unwrap();
+    let x = parser::parse_expr("y + z").unwrap();
     let actual = calc!("2 * $x + 1");
-    let expected = Expr::Add(
-        Box::new(Expr::Mul(
-            Box::new(Expr::Const(2)),
-            Box::new(Expr::Add(
-                Box::new(Expr::Var("y".to_string())),
-                Box::new(Expr::Var("z".to_string())),
-            )),
-        )),
-        Box::new(Expr::Const(1)),
-    );
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_nested_antiquote() {
+    let expected = parser::parse_expr("2 * (y + z) + 1").unwrap();
     let x = calc!("y + z");
     let actual = calc!("2 * $x + 1");
-    let expected = Expr::Add(
-        Box::new(Expr::Mul(
-            Box::new(Expr::Const(2)),
-            Box::new(Expr::Add(
-                Box::new(Expr::Var("y".to_string())),
-                Box::new(Expr::Var("z".to_string())),
-            )),
-        )),
-        Box::new(Expr::Const(1)),
-    );
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_complex_expression() {
+    let expected = parser::parse_expr("2 * (x + 1) - 3 * (y - 4)").unwrap();
     let actual = calc!("2 * (x + 1) - 3 * (y - 4)");
-    let expected = Expr::Sub(
-        Box::new(Expr::Mul(
-            Box::new(Expr::Const(2)),
-            Box::new(Expr::Add(Box::new(Expr::Var("x".to_string())), Box::new(Expr::Const(1)))),
-        )),
-        Box::new(Expr::Mul(
-            Box::new(Expr::Const(3)),
-            Box::new(Expr::Sub(Box::new(Expr::Var("y".to_string())), Box::new(Expr::Const(4)))),
-        )),
-    );
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_deeply_nested_parens() {
+    let expected = parser::parse_expr("(((x + 1) * 2) + ((y - 3) * 4))").unwrap();
     let actual = calc!("(((x + 1) * 2) + ((y - 3) * 4))");
-    let expected = Expr::Add(
-        Box::new(Expr::Mul(
-            Box::new(Expr::Add(Box::new(Expr::Var("x".to_string())), Box::new(Expr::Const(1)))),
-            Box::new(Expr::Const(2)),
-        )),
-        Box::new(Expr::Mul(
-            Box::new(Expr::Sub(Box::new(Expr::Var("y".to_string())), Box::new(Expr::Const(3)))),
-            Box::new(Expr::Const(4)),
-        )),
-    );
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_negation() {
+    let expected = parser::parse_expr("-x").unwrap();
     let actual = calc!("-x");
-    let expected = Expr::Neg(Box::new(Expr::Var("x".to_string())));
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_negation_with_parens() {
+    let expected = parser::parse_expr("-(x + y)").unwrap();
     let actual = calc!("-(x + y)");
-    let expected = Expr::Neg(Box::new(Expr::Add(
-        Box::new(Expr::Var("x".to_string())),
-        Box::new(Expr::Var("y".to_string())),
-    )));
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_double_negation() {
+    let expected = parser::parse_expr("--x").unwrap();
     let actual = calc!("--x");
-    let expected = Expr::Neg(Box::new(Expr::Neg(Box::new(Expr::Var("x".to_string())))));
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_exponentiation() {
+    let expected = parser::parse_expr("x ^ 2").unwrap();
     let actual = calc!("x ^ 2");
-    let expected = Expr::Exp(Box::new(Expr::Var("x".to_string())), Box::new(Expr::Const(2)));
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_complex_exponentiation() {
+    let expected = parser::parse_expr("(x + 1) ^ (y - 2)").unwrap();
     let actual = calc!("(x + 1) ^ (y - 2)");
-    let expected = Expr::Exp(
-        Box::new(Expr::Add(Box::new(Expr::Var("x".to_string())), Box::new(Expr::Const(1)))),
-        Box::new(Expr::Sub(Box::new(Expr::Var("y".to_string())), Box::new(Expr::Const(2)))),
-    );
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_precedence() {
+    let expected = parser::parse_expr("x + y * z").unwrap();
     let actual = calc!("x + y * z");
-    let expected = Expr::Add(
-        Box::new(Expr::Var("x".to_string())),
-        Box::new(Expr::Mul(
-            Box::new(Expr::Var("y".to_string())),
-            Box::new(Expr::Var("z".to_string())),
-        )),
-    );
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_exponentiation_precedence() {
+    let expected = parser::parse_expr("x * y ^ z").unwrap();
     let actual = calc!("x * y ^ z");
-    let expected = Expr::Mul(
-        Box::new(Expr::Var("x".to_string())),
-        Box::new(Expr::Exp(
-            Box::new(Expr::Var("y".to_string())),
-            Box::new(Expr::Var("z".to_string())),
-        )),
-    );
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_multiple_antiquotes() {
+    let expected = parser::parse_expr("(a + b) - (c * d)").unwrap();
     let x = calc!("a + b");
     let y = calc!("c * d");
     let actual = calc!("$x - $y");
-    let expected = Expr::Sub(
-        Box::new(Expr::Add(
-            Box::new(Expr::Var("a".to_string())),
-            Box::new(Expr::Var("b".to_string())),
-        )),
-        Box::new(Expr::Mul(
-            Box::new(Expr::Var("c".to_string())),
-            Box::new(Expr::Var("d".to_string())),
-        )),
-    );
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_complex_with_multiple_antiquotes() {
+    let expected = parser::parse_expr("((a + b) ^ 2) - ((c * d) + 3)").unwrap();
     let x = calc!("a + b");
     let y = calc!("c * d");
     let actual = calc!("($x ^ 2) - ($y + 3)");
-    let expected = Expr::Sub(
-        Box::new(Expr::Exp(
-            Box::new(Expr::Add(
-                Box::new(Expr::Var("a".to_string())),
-                Box::new(Expr::Var("b".to_string())),
-            )),
-            Box::new(Expr::Const(2)),
-        )),
-        Box::new(Expr::Add(
-            Box::new(Expr::Mul(
-                Box::new(Expr::Var("c".to_string())),
-                Box::new(Expr::Var("d".to_string())),
-            )),
-            Box::new(Expr::Const(3)),
-        )),
-    );
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_single_constant() {
+    let expected = parser::parse_expr("42").unwrap();
     let actual = calc!("42");
-    let expected = Expr::Const(42);
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_single_variable() {
+    let expected = parser::parse_expr("variable").unwrap();
     let actual = calc!("variable");
-    let expected = Expr::Var("variable".to_string());
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_single_antiquote() {
+    let expected = parser::parse_expr("x * y").unwrap();
     let expr = calc!("x * y");
     let actual = calc!("$expr");
-    let expected =
-        Expr::Mul(Box::new(Expr::Var("x".to_string())), Box::new(Expr::Var("y".to_string())));
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_chained_operations() {
+    let expected = parser::parse_expr("a + b + c + d").unwrap();
     let actual = calc!("a + b + c + d");
-    // Due to left associativity, this should be ((a + b) + c) + d
-    let expected = Expr::Add(
-        Box::new(Expr::Add(
-            Box::new(Expr::Add(
-                Box::new(Expr::Var("a".to_string())),
-                Box::new(Expr::Var("b".to_string())),
-            )),
-            Box::new(Expr::Var("c".to_string())),
-        )),
-        Box::new(Expr::Var("d".to_string())),
-    );
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_mixed_chained_operations() {
+    let expected = parser::parse_expr("a + b * c - d").unwrap();
     let actual = calc!("a + b * c - d");
-    let expected = Expr::Sub(
-        Box::new(Expr::Add(
-            Box::new(Expr::Var("a".to_string())),
-            Box::new(Expr::Mul(
-                Box::new(Expr::Var("b".to_string())),
-                Box::new(Expr::Var("c".to_string())),
-            )),
-        )),
-        Box::new(Expr::Var("d".to_string())),
-    );
     assert_eq!(expected, actual);
 }
